0b3f6b69291720ac190e59270b7d81dd41faef71
Valeiy Yudaev <tec.valery@gmail.com>
2019-11-10 18:06:05 +0300

    Minor refs2

--- src/test/java/com/example/detail/DetailServiceTest.java
+++ src/test/java/com/example/detail/DetailServiceTest.java
@@ -1,13 +1,14 @@
 package com.example.detail;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ToolApplicationTests {
+class DetailServiceTest {
 
+	@DisplayName("Initial empty test")
 	@Test
-	void contextLoads() {
+	void helloTest() {
 	}
-
 }
