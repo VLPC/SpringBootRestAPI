6d8b5ae7d4f713322b54f15839842abcb9533ae3
VLPC <vladimirngladkov@gmail.com>
2019-11-12 10:05:07 +0500

    add ModelMapper

--- src/main/java/com/example/detail/mapper/Mapper.java
+++ src/main/java/com/example/detail/mapper/Mapper.java
@@ -2,16 +2,21 @@ package com.example.detail.mapper;
 
 import com.example.detail.dto.DetailDto;
 import com.example.detail.model.Detail;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Mapper {
 
+    @Autowired
+    ModelMapper modelMapper;
+
     public Detail toEntity(DetailDto detailDto){
-        return new Detail(detailDto.getArticle(), detailDto.getDateCreated());
+        return modelMapper.map(detailDto, Detail.class);
     }
 
     public DetailDto toDto(Detail detail){
-        return new DetailDto(detail.getArticle(), detail.getDateCreated());
+        return modelMapper.map(detail, DetailDto.class);
     }
 }
