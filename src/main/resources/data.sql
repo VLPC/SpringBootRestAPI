3722f81dcc6f0dda06c821cc1ddbe48ac0648db3
VLPC <vladimirngladkov@gmail.com>
2019-11-14 15:07:14 +0500

    udate settings

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO organisation (id, name, address) VALUES ('1', 'name', 'address');
+
+INSERT INTO detail (id, article, prod, organisation_id) VALUES ('1', 'article1', '2019-10-10', '1'), ('2', 'article2', '2017-10-10', '1'), ('3', 'article3', '2917-10-10', '1'), ('4', 'article4', '2047-10-10', '1'),
+                                                                ('5', 'article5', '2015-10-10', '1'), ('6', 'article6', '2016-11-10', '1'), ('7', 'article7', '2066-10-10', '1'), ('8', 'article8', '2027-10-10', '1'),
+                                                                ('9', 'article9', '2014-10-10', '1'), ('10', 'article10', '2021-10-10', '1'), ('11', 'article11', '2010-10-10', '1'), ('12', 'article12', '2099-10-10', '1');
